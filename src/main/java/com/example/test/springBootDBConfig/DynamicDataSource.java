d792e7c75a2eeb52adcd9bd090cda19607a369d3
LTFGupo <m13732299527@163.com>
2018-06-14 16:46:15 +0800

    132

--- src/main/java/com/example/test/springBootDBConfig/DynamicDataSource.java
+++ src/main/java/com/example/test/springBootDBConfig/DynamicDataSource.java
@@ -2,15 +2,9 @@ package com.example.test.springBootDBConfig;
 
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
-import javax.sql.DataSource;
 
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
-//    @Override
-//    protected Object determineCurrentLookupKey() {
-//        System.out.println("--->数据源为--->  " + DataSourceContextHolder.getDB());
-//        return DataSourceContextHolder.getDB();
-//    }
 
     @Override
     protected Object determineCurrentLookupKey() {
